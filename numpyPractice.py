d1a70cbb22b360c45a40a163cf15a99ad63f29b8
Muhammad Imtiaz Anwar <31131790+imtiaz70@users.noreply.github.com>
2022-05-26 20:07:37 +0500

    numpyAdvance

--- numpyPractice.py
+++ numpyPractice.py
@@ -5,11 +5,8 @@ y = np.arange(7)
 
 l = np.array([3,5,7] , dtype=complex)
 m = np.array([10,20,30] , dtype=complex)
-# x = x.reshape(3,4)
-print(l.real)
-print(np.angle(m))
 
-
-
-# print("Mod of x , y " , np.remainder(l,m))
+o = np.arange(12)
+print(o)
+o = o.reshape(3,4)
 
